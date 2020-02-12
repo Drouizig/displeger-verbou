e30034174304a34d93c21ec2fc91a5e3466f2a13
Tornoz <tornoz@gozmail.bzh>
2020-02-12 18:15:01 +0100

    fix csv output in urzhian.py

--- scripts/urzhian.py
+++ scripts/urzhian.py
@@ -4,8 +4,10 @@ import csv
 import operator
 
 reader = csv.reader(open(sys.argv[1]), delimiter=";")
+writer = csv.writer(sys.stdout, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
 next(reader)
 sortedlist = sorted(reader, key = operator.itemgetter(2,0,1), reverse=False)
 print("anv_verb;diaz_verb;rummad;galleg;saozneg")
 for row in sortedlist:
-    print(';'.join(row))
+    writer.writerow(row)
